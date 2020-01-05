ff013a393b384be6d20f7b8da039eb7eb16d0a2d
Samn <san.negi.77777@gmail.com>
2020-01-05 19:37:06 +0530

    auth embed

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 
+
 var client=io();
 var username='user';
+//auth strt
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBm4-z34aUVAsVCoNBG_BW7jRras2t2Q8c",
+    authDomain: "auth-38516.firebaseapp.com",
+    databaseURL: "https://auth-38516.firebaseio.com",
+    projectId: "auth-38516",
+    storageBucket: "auth-38516.appspot.com",
+    messagingSenderId: "372230674711",
+    appId: "1:372230674711:web:3e1b7ca6d0a08cf2cdbf52",
+    measurementId: "G-ECKT7FTBKH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+
+
+function login(){
+    $('.username_page ').hide()
+
+    function newlogin(user){
+        if(user){
+            app(user);
+        }
+        else{
+            var provider=new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithRedirect(provider);
+
+        }
+    }
+ firebase.auth().onAuthStateChanged(newlogin)
+}
+
+function app(user){
+    $('.username_page ').show()
+    console.log(user.displayName)
+    
+}
+  window.onload= login
+
+
+//socket strt
  $('#message').on('input',()=>{ 
     let msg= $('#message').val();
     client.emit('msgdisplay',{username:username,msg:msg})
